aadd1f35da395211eebfb922dfc42a5a9a0bc990
marwai <mtse@spartaglobal.com>
2020-08-19 16:10:01 +0100

    adding detailed notes on readme.md

--- setup.sh
+++ setup.sh
@@ -9,10 +9,11 @@ sudo apt-get install tree -y
 sudo apt-add-repository ppa:ansible/ansible;
 sudo apt-get install ansible -y
 
+echo "AWS setup completed"
 
-
+sudo su
 cd /etc/ansible
-sudo nano hosts
+
 echo "[web]
 192.168.33.10 ansible_connection=ssh ansible_ssh_user=vagrant ansible_ssh_pass=vagrant" >> hosts
 
@@ -24,17 +25,26 @@ echo "[aws]
 
 cd ..
 
-#go into web server
-ssh vagrant@192.168.33.11
+echo "EXPORTING HOSTS TO ENVIRONMENT COMPLETED"
+
+# go into web server
+sshpass -p 'vagrant' ssh vagrant@192.168.33.10
 sudo apt-get update -y
 sudo apt-get upgrade -y
+echo "WEB IS DONE"
 exit
 
-#go into dev server
-ssh vagrant@192.168.33.12
+# go into db server
+sshpass -p 'vagrant' ssh vagrant@192.168.33.11
 sudo apt-get update -y
 sudo apt-get upgrade -y
+echo "DATABASE IS DONE"
 exit
+EOF
 
-ansible all -m ping
+# go into aws server
+ssh vagrant@192.168.33.12 << EOF
+cd /etc/ansible
+ansible-playbook install.yml
+exit
 EOF
